5e57543c6ed09dbbbc15280beb52de23c2d8bbf6
amir-memarian <memarian.email@gmail.com>
2025-11-27 19:23:42 +0330

    Static Method

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MathTest {
+    @Test
+    void Bayad_amaliat_Riazi_Anjam_Dahad() {
+        double moteghayer = -1.5;
+
+        double meghdarMotlagh = Math.abs(moteghayer);
+
+        Assertions.assertEquals(1.5,meghdarMotlagh);
+    }
+}
