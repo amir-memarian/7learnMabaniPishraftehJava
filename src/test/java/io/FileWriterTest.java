a1b36bd739866fd0b681c67a15c08c92cdabf7ef
amir-memarian <memarian.email@gmail.com>
2025-12-01 19:11:36 +0330

    fileInputStream

--- src/test/java/io/FileWriterTest.java
+++ src/test/java/io/FileWriterTest.java
@@ -1,17 +1,33 @@
 package io;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class FileWriterTest {
+
+    public static final String FILENAME = "test.txt";
+    public static final String SENTENCE = "Hello World";
+
     @Test
     void should_write_in_a_file() {
-        try(FileOutputStream fileOutputStream = new FileOutputStream("test.txt")) {
-            fileOutputStream.write("Hello world".getBytes(StandardCharsets.UTF_8));
+        try(FileOutputStream fileOutputStream = new FileOutputStream(FILENAME);
+            FileInputStream fileInputStream = new FileInputStream(FILENAME)) {
+
+            fileOutputStream.write(SENTENCE.getBytes(StandardCharsets.UTF_8));
+
+            int read;
+            StringBuilder helloWorld = new StringBuilder();
+            while ((read =fileInputStream.read()) != -1){
+            //    System.out.print((char) read);
+                 helloWorld.append((char) read);
+            }
+
+            Assertions.assertThat(helloWorld.toString()).isEqualTo("Hello World");
 
         } catch (IOException e) {
             System.out.println("there is a problem with test.txt file");
@@ -42,4 +58,5 @@ public class FileWriterTest {
         }
         */
     }
+
 }
