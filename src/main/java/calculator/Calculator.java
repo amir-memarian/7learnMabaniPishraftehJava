a3e0dcdf158713c5d251d6de2e961f2be53d94e2
amir-memarian <memarian.email@gmail.com>
2025-12-01 17:17:41 +0330

    try-catch - throw

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,8 +3,15 @@ package calculator;
 public class Calculator {
 
     public String add(final String adadAval, final String adadDovom) {
-        final double num1 = Double.parseDouble(adadAval);
-        final double num2 = Double.parseDouble(adadDovom);
+        double num1,num2;
+        try{
+            num1 = Double.parseDouble(adadAval);
+            num2 = Double.parseDouble(adadDovom);
+
+        } catch (NumberFormatException nfe){
+            System.out.println("Adade vared shode ghabel gabol nist.");
+            throw nfe;
+        }
         final double result = num1 + num2;
         return String.valueOf(result);
     }
